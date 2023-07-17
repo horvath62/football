1a1ad89eaf55bf150de2bc8754b2002a4a4303dd
Stephen Horvath <steve.horvath@hpe.com>
2023-07-17 13:23:12 -0700

    intial commit

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, NoSuchAttributeException, TimeoutException, ElementClickInterceptedException
 
 import time
+
+exception = False
 
 driver = webdriver.Chrome()
 
@@ -17,25 +21,48 @@ for play in PlayList:
     print(play.text)
 '''
 
-time.sleep(3) # Sleep for 3 seconds
+time.sleep(0.5) # Sleep
 
 # Collapse first drive playlist (because it default is expanded, remaining playlists are collapsed)
-#DriveFirst = driver.find_element(By.CLASS_NAME, "AccordionHeader")
-DriveFirst = driver.find_element(By.CLASS_NAME, "AccordionPanel.Panel")
-D1 = DriveFirst.find_element(By.CLASS_NAME, 'AccordionHeader__Left')
-D1.click()
-# print("#", D1.text)
+try:
+    DriveFirst = driver.find_element(By.CLASS_NAME, "AccordionPanel.Panel")
+    D1 = DriveFirst.find_element(By.CLASS_NAME, 'AccordionHeader__Left')
+    D1.click()
+except NoSuchElementException:
+    print("Exception: No Such Element")
+    exception = True
+except ElementClickInterceptedException:
+    print("Exception: Element Click Intercepted")
+    exception = True
+
 
 # Find all the drives
-DriveList = driver.find_elements(By.CLASS_NAME, 'AccordionPanel.Panel')
+try:
+    DriveList = driver.find_elements(By.CLASS_NAME, 'AccordionPanel.Panel')
+except NoSuchElementException:
+    print("Exception: No Such Element")
+    exception = True
+except ElementClickInterceptedException:
+    print("Exception: Element Click Intercepted")
+    exception = True
 
-# Now expand all play lists by clicking the drive header
+# Now expand all play lists in each drive by clicking the drive header
 for drive in DriveList:
-    driveHeader = drive.find_element(By.CLASS_NAME, 'AccordionHeader__Left')
-    driveHeader.click()
-    time.sleep(1)
+    try:
+        driveHeader = drive.find_element(By.CLASS_NAME, 'AccordionHeader__Left')
+        driveHeader.click()
+    except NoSuchElementException:
+        print("Exception: No Such Element")
+        exception = True
+    except ElementClickInterceptedException:
+        print("Exception: Element Click Intercepted")
+        print("Try increasing sleep time between clicks to allow html page to re-render")
+        exception = True
+    time.sleep(0.2)
+    print("##", DriveList)
 
-# Now get Headline, description and plays of each drive
+
+# Now get Headline & description of each drive and the plays of each drive
 for drive in DriveList:
     DriveHeadline = drive.find_element(By.CLASS_NAME, 'AccordionHeader__Left__Drives__Headline')
     DriveDesc = drive.find_element(By.CLASS_NAME, 'AccordionHeader__Left__Drives__Description')
@@ -43,12 +70,12 @@ for drive in DriveList:
     print("###",DriveHeadline.text,"->",new_string)
     PlayList = drive.find_elements(By.CLASS_NAME, 'PlayListItem')
     new_string = ', '.join((drive.text).split('\n'))
-    print("##",new_string)
+    # print("##",new_string)
     for play in PlayList:
         new_string = ', '.join((play.text).split('\n'))
         print("#",play.text)
 
-
+'''
 '''
 try:
     PlayList = driver.find_elements(By.CLASS_NAME, 'PlayListItem')
@@ -60,8 +87,12 @@ for play in PlayList:
     print(play.text)
 '''
 
+if exception == True:
+    print("EXCEPTION WAS HANDLED:  DATA IS INCOMPLETE")
+
 
 time.sleep(10) # Sleep for 3 seconds
+
 
 driver.quit()
 
