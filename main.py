e6aa60176bbd4c756f9a4d72fde38b0aca900841
Stephen Horvath <steve.horvath@hpe.com>
2023-07-17 16:14:09 -0700

    raw scraping working

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ except ElementClickInterceptedException:
     exception = True
 
 
-# Find all the drives
+# Find all the "football" drives, put into DriveList
 try:
     DriveList = driver.find_elements(By.CLASS_NAME, 'AccordionPanel.Panel')
 except NoSuchElementException:
@@ -66,8 +66,8 @@ for drive in DriveList:
 for drive in DriveList:
     DriveHeadline = drive.find_element(By.CLASS_NAME, 'AccordionHeader__Left__Drives__Headline')
     DriveDesc = drive.find_element(By.CLASS_NAME, 'AccordionHeader__Left__Drives__Description')
-    new_string = ', '.join((DriveDesc.text).split('\n'))
-    print("###",DriveHeadline.text,"->",new_string)
+    new_desc = ', '.join((DriveDesc.text).split('\n'))
+    print("###",DriveHeadline.text,"->",new_desc)
     PlayList = drive.find_elements(By.CLASS_NAME, 'PlayListItem')
     new_string = ', '.join((drive.text).split('\n'))
     # print("##",new_string)
@@ -85,7 +85,7 @@ except NoSuchElementException:
 
 for play in PlayList:
     print(play.text)
-'''
+
 
 if exception == True:
     print("EXCEPTION WAS HANDLED:  DATA IS INCOMPLETE")
@@ -97,70 +97,3 @@ time.sleep(10) # Sleep for 3 seconds
 driver.quit()
 
 
-
-'''
-driver.find_element_by_xpath('//div[@class="css-ais6tt"]//button[3]')
-                              //*[text()='Começa a new game']
-link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='p-5']/div[4]/button[1]")))
-
-<input type="text" name="passwd" id="passwd-id" />
-element = driver.find_element(By.ID, "passwd-id")
-element = driver.find_element(By.NAME, "passwd")
-element = driver.find_element(By.XPATH, "//input[@id='passwd-id']")
-element = driver.find_element(By.CSS_SELECTOR, "input#passwd-id")
-XPATH only the first will be returned. if none: a NoSuchElementException will be raised.
-
-element.click()
-
-
-<html>
- <body>
-  <form id="loginForm">
-   <input name="username" type="text" />
-   <input name="password" type="password" />
-   <input name="continue" type="submit" value="Login" />
-  </form>
- </body>
-<html>
-
-login_form = driver.find_element(By.XPATH, "/html/body/form[1]")
-login_form = driver.find_element(By.XPATH, "//form[1]")
-
-try:
-    driver.find_element(By.XPATH, "(//*[contains(@class, 'needsclick')]//div[@role='dialog']//button)[1]").click()
-except:
-    print('modal not shown')
-
-
-'''
-
-
-
-'''
-import requests
-from requests_html import HTMLSession
-
-import certifi
-print (certifi.where())
-
-# url = 'https://www.espn.com/college-football/playbyplay/_/gameId/401019470'
-# url = 'https://www.espn.com/college-football/scoreboard'
-url = 'https://python.org/'
-
-try:
-    session = HTMLSession()
-    response = session.get(url, verify=False)
-
-except requests.exceptions.RequestException as e:
-    print("exception:", e)
-
-
-
-title=response.html.find('title')
-print(title)
-
-
-fname = football.html
-obj = requests.get(url)
-open(temp.html, 'w')
-'''
