50ef87de7fc2e54d471ff19a3b3d280579e16d03
Stephen Horvath <steve.horvath@hpe.com>
2023-07-19 10:57:11 -0700

    raw scraping working

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from scrape_plays import scrape
 
+gameID = 262432084
+
+
 s = scrape()
-exc = s.scrapeplays(262432084, 0.2)
+exc = s.scrapeplays(gameID, 0.2)
 print("Exception Raised?", exc)
+
+
+filename = 'scrape' + str(gameID)
+
+f = open(filename, 'w' )
+
+for line in s.rawplaylist:
+    f.write( line + '\n')
+
+f.close()
